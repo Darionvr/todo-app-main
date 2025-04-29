2112d37220e09dd922142668b0939b33fca500a4
Nico Velasco <nicolasvelasco.r@gmail.com>
2025-04-29 17:34:40 -0400

    Updated Fetch url

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [filter, setFilter] = useState("all");
 
   const getTodos = async () => {
-    const response = await fetch('http://localhost:3000/')
+    const response = await fetch('todo-app-main-production.up.railway.app/')
     const todos = await response.json();
     setTodos(todos)
   }
@@ -19,7 +19,7 @@ function App() {
 
 
   const addTodo = async (title) => {
-    const response = await fetch('http://localhost:3000/', {
+    const response = await fetch('todo-app-main-production.up.railway.app', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title }),
@@ -38,7 +38,7 @@ function App() {
 
 
   const removeTodo = async (id) => {
-    const response = await fetch(`http://localhost:3000/${Number(id)}`, {
+    const response = await fetch(`todo-app-main-production.up.railway.app${Number(id)}`, {
       method: "DELETE",
     });
     if (response.status !== 200) {
@@ -48,7 +48,7 @@ function App() {
   }
 
   const updateTodo = async (id) => {
-    const response = await fetch(`http://localhost:3000/${id}`, {
+    const response = await fetch(`todo-app-main-production.up.railway.app${id}`, {
       method: "PUT",
     });
     if (response.status !== 200) {
@@ -65,7 +65,7 @@ function App() {
   }
 
   const clearCompleted = async () => {
-    const response = await fetch('http://localhost:3000/clear-completed', {
+    const response = await fetch('todo-app-main-production.up.railway.app/clear-completed', {
       method: "DELETE",
     });
 
