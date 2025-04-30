eec1050e1ce86616c6287ce64c8a49a2cc08cc0c
Nico Velasco <nicolasvelasco.r@gmail.com>
2025-04-30 00:25:12 -0400

    Todo de nuevo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-      proxy: {
-          '/api': {
-              target: 'http://127.0.0.1:3000', // Cambia localhost por 127.0.0.1
-              changeOrigin: true,
-          },
-      },
-  },
-});
+    proxy: {
+        '/api': 'http://localhost:3000', 
+    },
+},
+})
